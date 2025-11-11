1518a21d4fd0f263218abbe49bc35dbd3cdaeeef
Felipe Augusto da Silva <felipe.silva@corp.tapetessaocarlos.com.br>
2025-11-11 07:24:06 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App(){
+
+  const addUser = async () => {
+    
+    try {
+      const response =  await fetch("http://localhost:3000/users", 
+        {
+          method : "POST",
+          headers : {"Content-Type":"application/json"},
+          body: JSON.stringify(
+            {
+            "name":"Felipe Augusto",
+            "email":"lipe.augusto@gmail.com"
+          }
+          )
+        }
+      )
+      
+      const data = await response.json();
+      console.log("Usuario Criado!");
+
+    } catch (error) {
+
+      console.error("ERROR: Erro ao adicionar", error);
+
+    }
+    
+  }
+
+  return <button  onClick={addUser}>Adicionar</button>
 }
-
-export default App;
